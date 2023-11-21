4242efc1d690dcb869801d7299f158252028a69e
Matthew Twede <maximoolean@gmail.com>
2023-11-21 12:11:30 -0700

    merging other pages to my branch NOT FINAL

--- server.js
+++ server.js
@@ -7,9 +7,17 @@ const path = require('path');
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 app.set('layout', './layouts/layout'); // not at views root
+
 app.get('/', function (req, res) {
   res.render('index', { title: 'Home' });
+});
+app.get('/views/cart.ejs', function (req, res) {
+  res.render('cart', { title: 'Cart' }); // Renders page2.ejs 
+});
+app.get('/views/productPage.ejs', function (req, res) {
+  res.render('productPage', { title: 'Products' }); // Renders page2.ejs 
 });
 
 /* ***********************
